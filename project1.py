247d1e2c23f6cf89b3dee639da1dc83c169ce311
golakiya123 <golakiyavenky@gmail.com>
2026-08-13 17:20:57 +0530

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,29 @@
+print("welcome to the interactive personal Data collector!")
+
+name=(input("\n Please enter your name:"))
+age=int(input("\n Please enter your age:"))
+height=(input("\n Please enter your height in meters:"))
+Favourit_number=int(input("\n Please enter your Favourit number:"))
+
+
+print("\n Thank You! Here is the information collected:")
+print("\n")
+        
+
+Name=str(input("Enter Your Name:"))
+Age=int(input("Enter Your Age:"))
+Height=float(input("Enter Your Height:"))
+Favourit_number=int(input("Enter Your Favourit number:"))
+birth_year=2026-Age
+
+print("Name:",Name,"type:",type(Name),"id:(Adress)",id(Name))
+print("Age:",Age,"type:",type(Age),"id:(Adress)",id(Age))
+print("Height:",Height,"type:",type(Height),"id:(Adress)",id(Height))
+print("Favourit_number:",Favourit_number,"type:",type(Favourit_number),"id:(Adress)",id(Favourit_number))
+
+
+print("Your Birth Year is approximately:", birth_year)
+
+  
+print("\n")
+print("Thank you For using the personal Data collector. Goodbye!")
